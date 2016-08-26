10c95cd31e00caa12ffbadcbbc54c5f711fb52cf
DanielD <danidiaza@gmail.com>
2016-08-26 12:39:32 -0600

    Revert "SMUL operation"
    This reverts commit 1baa354bbad084fc22d00e1124057c432a159831.
    
    Esto iba en la carpeta del experimento 2

--- Experimento1/Defintions.v
+++ Experimento1/Defintions.v
@@ -9,8 +9,7 @@
 `define STO   4'd4
 `define ADD   4'd5
 `define JMP   4'd6
-`define SUB   4'd7	// Resta
-`define SMUL   4'd8	// Multiplicacion con signo
+`define SUB   4'd7
 
 `define R0 8'd0
 `define R1 8'd1
